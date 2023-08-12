385a11993a5431b3dfa78802c55b8987b5b9bae2
Piyush Agrawal <113158234+piyushagrawal0330@users.noreply.github.com>
2023-08-12 16:54:24 +0530

    Time: 65 ms (67.93%), Space: 48 MB (27.20%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String sortVowels(String s) {
-        String ans = "";
+        StringBuffer ans = new StringBuffer();;
         List<Character> list = new ArrayList<>();
         
         for(int i=0;i<s.length();i++){
@@ -11,10 +11,10 @@ class Solution {
         Collections.sort(list);
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
-                ans += list.get(c++);
+                ans.append(list.get(c++));
             else
-                ans+=s.charAt(i);
+                ans.append(s.charAt(i));
         }
-        return ans;
+        return ans.toString();
     }
 }
