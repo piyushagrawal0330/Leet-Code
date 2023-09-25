1e20b0b94f64c9a28cb3f2674da76c93e6528cb2
Piyush Agrawal <113158234+piyushagrawal0330@users.noreply.github.com>
2023-09-25 22:47:03 +0530

    Time: 0 ms (100.00%), Space: 40 MB (96.22%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length())
+            return false;
+        String helper = s+s;
+        return helper.contains(goal);
+    }
+}
